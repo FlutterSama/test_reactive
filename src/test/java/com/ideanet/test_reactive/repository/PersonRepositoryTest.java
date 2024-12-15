4a7e32ad7d04dcb58442a167bee53f8198f19f7a
MinThuKyaw <minthukyaw168@gmail.com>
2024-12-15 18:59:51 +0630

    test mono and flux

--- src/test/java/com/ideanet/test_reactive/repository/PersonRepositoryTest.java
+++ src/test/java/com/ideanet/test_reactive/repository/PersonRepositoryTest.java
@@ -4,8 +4,11 @@ import com.ideanet.test_reactive.domain.Person;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PersonRepositoryTest {
     PersonRepository personRepository = new PersonRepositoryImpl();
@@ -53,5 +56,47 @@ public class PersonRepositoryTest {
         Flux<Person> personFlux = personRepository.findAll();
         Mono<List<Person>> listMono = personFlux.collectList();
         listMono.subscribe(list -> list.forEach(System.out::println));
+    }
+
+    @Test
+    void testFilterFlux(){
+        personRepository.findAll()
+                .filter(person -> person.getFirstName().equals("kyaw"))
+                .subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testGetById(){
+        Mono<Person> personMono = personRepository.findAll()
+                .filter(person -> person.getFirstName().equals("kyaw"))
+                .next();
+        personMono.subscribe(person -> System.out.println(person.getFirstName()));
+    }
+
+    @Test
+    void testFindPersonByIdNotFound(){
+        Flux<Person> personMono = personRepository.findAll();
+        final int id = 8;
+        Mono<Person> person = personMono.filter(p -> p.getId() == id).single()
+                .doOnError(throwable -> {
+                    System.out.println("Person not found");
+                    System.out.println(throwable.toString());
+                });
+        person.subscribe(System.out::println, Throwable::printStackTrace);
+    }
+
+
+    @Test
+    void testGetByIdFound(){
+        Mono<Person> personMono = personRepository.findById(3);
+
+        assertTrue(personMono.hasElement().block());
+    }
+
+    @Test
+    void testGetByIdFoundStepVerifier(){
+        Mono<Person> personMono = personRepository.findById(3);
+
+        StepVerifier.create(personMono).expectNextCount(1).verifyComplete();
     }
 }
